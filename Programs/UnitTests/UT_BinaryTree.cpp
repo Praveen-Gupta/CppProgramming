0e79591ece6d38aa79eae6837093fe8dabaf45e2
Praveen Gupta <pravs.gupta@gmail.com>
2017-09-03 21:04:29 +0530

    Unit Test Project

---
+++ Programs/UnitTests/UT_BinaryTree.cpp
@@ -0,0 +1,24 @@
+#include "CppUnitTest.h"
+#include "..\BinaryTree\BinaryTree.h"
+
+#include <cassert>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(BinaryTreeTests)
+	{
+	public:
+		
+		TEST_METHOD(BinaryTree_Height)
+		{
+			BinaryTree MyTree;
+
+			MyTree.AddMultipleNode({ 100,50,200,1,-1,400,180,40,60,150,250 });
+
+			assert(MyTree.height(MyTree.m_pRoot) == 4);
+		}
+
+	};
+}
